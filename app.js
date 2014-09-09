e1a0947bc168d257270d910851ae02fe996e3e21
zhangekehbg <itzhangke@gmail.com>
2014-09-09 11:20:18 +0800

    实现界面之间的跳转

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var MongoStore = require('connect-mongo')(express);
@@ -40,8 +39,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+routes(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 
-/*
- * GET home page.
- */
-
-exports.index = function(req, res){
-  res.render('index', { title: 'web_pos' });
+module.exports = function(app) {
+    app.get('/', function (req, res) {
+        res.render('index', { title: 'web_pos' });
+    });
+    app.get('/product_list', function (req, res) {
+        res.render('product_list', { title: 'web_pos' });
+    });
+    app.get('/shopping_cart', function (req, res) {
+        res.render('shopping_cart', { title: 'web_pos' });
+    });
 };
