e77e9e53c91dfc30a877282e0e10eea730a344f2
zhangekehbg <itzhangke@gmail.com>
2014-09-10 14:55:04 +0800

    添加商品信息shops到mongo数据库pos_node中，并且在商品列表打开数据库读取信息显示在页面上

---
+++ models/product_list.js
@@ -0,0 +1,33 @@
+/**
+ * Created by zk on 14-9-10.
+ */
+var mongodb = require('./db');
+var _ = require('../public/javascripts/underscore-min.js');
+
+function Product(){
+}
+
+module.exports = Product;
+
+Product.get = function(back){
+    mongodb.open(function(err,db){
+        if(err){
+            return back(err);
+        }
+        db.collection('shops',function(err,collection){
+            if(err){
+                mongodb.close();
+                return back(err);
+            }
+            collection.find({}).sort({
+                time:-1
+            }).toArray(function(err,shops){
+                mongodb.close();
+                if(err){
+                    return back(err);
+                }
+                back(null,shops);
+            });
+        });
+    });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,27 @@
+var Product = require('../models/product_list.js');
 
 module.exports = function(app) {
     app.get('/', function (req, res) {
-        res.render('index', { title: 'web_pos' });
+        if(!req.session.cart){
+            req.session.cart = [];
+        }
+        if(!req.session.total){
+            req.session.total = 0;
+        }
+        res.render('index',{title:"主页",total:req.session.total});
     });
     app.get('/product_list', function (req, res) {
-        res.render('product_list', { title: 'web_pos' });
+        Product.get(function(err,shoppings){
+            var shops = shoppings;
+            if(err){
+                shops = [];
+            }
+            res.render('product_list',{
+                total:req.session.total,
+                shops:shops
+            });
+        });
+
     });
     app.get('/shopping_cart', function (req, res) {
         res.render('shopping_cart', { title: 'web_pos' });
