f5566c8ddba4129ec339fe3fe46efd99d20eee20
zhangekehbg <itzhangke@gmail.com>
2014-10-13 13:57:20 +0800

    添加属性页:为日期添加验证,格式为2014-01-01

---
+++ public/javascripts/property.js
@@ -0,0 +1,14 @@
+/**
+ * Created by zk on 14-10-13.
+ */
+function verification (){
+    var current_form =  document.add_property;
+    if(current_form.property_name.value =='生产日期'){
+        if(current_form.property_value.value.match(/^((?:19|20)\d\d)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/)) {
+            return true;
+        } else {
+            alert('不是日期');
+        }
+    }
+    return false;
+}
